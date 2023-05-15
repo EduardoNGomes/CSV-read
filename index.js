d57bc9024c0479a0c1533c1892000dfa3f47467a
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-05-15 10:55:22 -0300

    Instal and Use csv-parse

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
+import { parse } from 'csv-parse'
 import { knex } from './knexfile.js'
-import neatCsv from 'neat-csv'
 import multer from 'multer'
 import fs from 'fs'
 
@@ -33,73 +33,70 @@ app.post('/test', upload.single('file'), async (req, res) => {
   const updatedData = []
   const resEntry = []
 
-  fs.readFile(`./${file.path}`, async (err, data) => {
-    if (err) {
-      console.log(err)
-      return
-    }
+  const csvPath = new URL(`./${file.path}`, import.meta.url)
 
-    const resEntryData = await neatCsv(data)
-    resEntry.push(resEntryData)
+  const stream = fs.createReadStream(csvPath)
+  const csvParse = parse({
+    delimiter: ',', // Separando por virgula
+    skip_empty_lines: true, // Pulando linhas vazias
+    from_line: 2 // Começando na segunda linha, pois a primeira é o cabeçalho
+  })
 
-    // code
-    // sales_price
-    // product_code
-    // new_price
+  const linesParse = stream.pipe(csvParse)
+  for await (const line of linesParse) {
+    const [product_code, new_price] = line
+    resEntry.push({ product_code, new_price })
+  }
 
-    for await (let itemEntry of resEntry) {
-      for await (let itemDB of responseDB) {
-        // Comparando codigo enviado pelo criando com codigo do produto do banco
-        if (itemEntry.product_code == JSON.parse(JSON.stringify(itemDB)).code) {
-          // Validando se o novo preco e maior do que o preco de custo
-          if (itemEntry.new_price < itemDB.cost_price) {
-            invalidEntries.push({
-              ...itemEntry,
-              message: 'Novo preço menor do que o valor de custo do produto'
-            })
-            break
-          }
-
-          // Validar se aumento e maior do que 10%
-          const itemWithTenPercent =
-            itemDB.sales_price + (itemDB.sales_price / 100) * 10
-
-          if (itemEntry.new_price > itemWithTenPercent) {
-            invalidEntries.push({
-              ...itemEntry,
-              message:
-                'Novo preço tem um aumento maior do que 10% do preço anterior'
-            })
-            break
-          }
-
-          // Validar se aumento e maior do que 10%
-          const itemWithoutTenPercent =
-            itemDB.sales_price - (itemDB.sales_price / 100) * 10
-          if (itemEntry.new_price < itemWithoutTenPercent) {
-            invalidEntries.push({
-              ...itemEntry,
-              message:
-                'Novo preço tem um aumento menor do que 10% do preço anterior'
-            })
-            break
-          }
-
-          updatedData.push({
-            code: itemDB.code,
-            name: itemDB.name,
-            current_price: itemDB.sales_price,
-            new_price: itemEntry.new_price
+  for await (let itemEntry of resEntry) {
+    for await (let itemDB of responseDB) {
+      // Comparando codigo enviado pelo criando com codigo do produto do banco
+      if (itemEntry.product_code == JSON.parse(JSON.stringify(itemDB)).code) {
+        // Validando se o novo preco e maior do que o preco de custo
+        if (itemEntry.new_price < itemDB.cost_price) {
+          invalidEntries.push({
+            ...itemEntry,
+            message: 'Novo preço menor do que o valor de custo do produto'
           })
+          break
         }
+
+        // Validar se aumento e maior do que 10%
+        const itemWithTenPercent =
+          itemDB.sales_price + (itemDB.sales_price / 100) * 10
+
+        if (itemEntry.new_price > itemWithTenPercent) {
+          invalidEntries.push({
+            ...itemEntry,
+            message:
+              'Novo preço tem um aumento maior do que 10% do preço anterior'
+          })
+          break
+        }
+
+        // Validar se aumento e maior do que 10%
+        const itemWithoutTenPercent =
+          itemDB.sales_price - (itemDB.sales_price / 100) * 10
+        if (itemEntry.new_price < itemWithoutTenPercent) {
+          invalidEntries.push({
+            ...itemEntry,
+            message:
+              'Novo preço tem um aumento menor do que 10% do preço anterior'
+          })
+          break
+        }
+
+        updatedData.push({
+          code: itemDB.code,
+          name: itemDB.name,
+          current_price: itemDB.sales_price,
+          new_price: itemEntry.new_price
+        })
       }
     }
-  })
-  setTimeout(() => {
-    console.log(updatedData, invalidEntries)
-    return res.send()
-  }, 500)
+  }
 
+  console.log(updatedData)
   return res.send(resEntry)
 })
 
